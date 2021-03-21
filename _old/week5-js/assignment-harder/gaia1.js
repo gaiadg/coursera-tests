92aca054920d277482a2026106f20ad79026e35e
gaiadg <gaia.digregorio@gmail.com>
2021-03-21 23:49:24 +0400

    I MADE IT

---
+++ _old/week5-js/assignment-harder/gaia1.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression
+// IIFE
+(function (name) {
+  console.log("Hello " + name);
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  console.log(names[1]);
+
+})
+("Coursera!");
+
+
+
+
