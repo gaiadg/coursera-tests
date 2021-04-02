a588444126009d820c7cb1f2f24a066829ea00a5
gaiadg <gaia.digregorio@gmail.com>
2021-04-02 18:13:12 +0400

    lol

---
+++ week6-sushi-withJS/js/script.js
@@ -0,0 +1,42 @@
+$(function() {
+  $("#navbarToggle").blur(function(event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+  $("#navbarToggle").click(function(event) {
+    $(event.target).focus();
+  });
+});
+
+
+// IIFE
+(function(global) {
+  var akira = {};
+  var homeHtml = "snippets/home-snippet.html";
+//  // adding html
+  var insertHtml = function(selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHtml = html;
+  };
+//  // loading icon
+  var showLoading = function(selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='img/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+    homeHtml,
+    function(responseText) {
+    document.querySelector("#main-content").innerHtml = responseText;
+    },
+    false);
+  });
+
+global.$akira = akira;
+
+}) (window);
